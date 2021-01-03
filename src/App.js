0134a56ebb015d752f54aa1f3be22cab1582cfb9
Jonathan Rubin <jonathan.rubin@honeywell.com>
2021-01-03 13:39:03 -0500

    setup to lint git staged files with lint-staged and husky

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="bg-blue-500">
-        Webpack 5 boilerplate with React 17, Tailwind 2, using babel, sass, with a hot dev server
-        and an optimized production build.
+        Webpack 5 boilerplate with React 17, Tailwind 2, using babel, sass, with
+        a hot dev server and an optimized production build.
       </div>
     );
   }
